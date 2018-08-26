44d8b00969fd4d7893ca17a4dca0a46ad653b552
sergiolizanamontero <slizana92@gmail.com>
2018-08-26 14:18:21 +0200

    Error in method observeViewModel in fragment

--- app/src/main/java/ikigaiworks/feedrss/view/MainFragment.kt
+++ app/src/main/java/ikigaiworks/feedrss/view/MainFragment.kt
@@ -9,6 +9,10 @@ import ikigaiworks.feedrss.api.observer.ApiObserver
 import ikigaiworks.feedrss.api.wrapper.DataWrapper
 import ikigaiworks.feedrss.model.FeedResponse
 import ikigaiworks.feedrss.viewmodel.FeedViewModel
+import com.sun.tools.internal.xjc.reader.xmlschema.bindinfo.BIConversion.User
+import ikigaiworks.feedrss.api.observer.ApiObserver
+
+
 
 
 
@@ -22,19 +26,19 @@ class MainFragment : Fragment(), LifecycleOwner{
         super.onCreate(savedInstanceState)
     }
 
-    fun observeViewModel(){
+    private fun observeViewModel(){
         viewModel?.searchFeedRss()?.observe(this, object: ApiObserver<DataWrapper<FeedResponse>>(object : ApiObserver.ChangeListener<DataWrapper<FeedResponse>>{
             override fun onSuccess(dataWrapper: DataWrapper<FeedResponse>?) {
-
+                TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
             }
 
             override fun onException(exception: Exception?) {
-
+                TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
             }
 
-        }))
-    }
+        })
 
+    }
 
 
 }
